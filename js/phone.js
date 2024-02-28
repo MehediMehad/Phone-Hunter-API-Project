1f7fc263444745fd7d6a87c5b8e9e8ecf1ae860a
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2024-02-28 13:35:09 +0600

    Search And Show Conditional Show All Button added

--- js/phone.js
+++ js/phone.js
@@ -7,12 +7,22 @@ const loadPhone = async (searchPhone) => {
 const desplayPhones = (phones) => {
   const phoneContainer = document.getElementById("phone-container");
   // clear phone container cards befor adding new card
-  phoneContainer.textContent=""
+  phoneContainer.textContent= ''
+  // display show all button if there are more than 12 phones
+  const showAllContainer = document.getElementById('showAllContainer')
+  if (phones.length > 12) {
+    showAllContainer.classList.remove('hidden')
+  }
+  else{
+    showAllContainer.classList.add('hidden')
+  }
+  // display only first 12 phones
+  phones = phones.slice(0, 12);
   phones.forEach((phone) => {
     // console.log(phone);
     // 2: create a div
     const phoneCard = document.createElement("div");
-    phoneCard.classList = `card bg-gray-200 p-4 shadow-xl`;
+    phoneCard.classList = `card bg-gray-100 p-4 shadow-xl border-2 hover:border-2 hover:border-sky-600`;
     // 3: set inner html
     phoneCard.innerHTML = `
         <figure><img src="${phone.image}" alt="Shoes" /></figure>
@@ -34,4 +44,3 @@ const handeleSearch = () =>{
   console.log(searchText);
   loadPhone(searchText)
 }
-// loadPhone();
